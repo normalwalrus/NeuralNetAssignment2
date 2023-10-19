4c19952865c950a804e8646a82c21405a8d13899
normalwalrus <ianchanjiajun@gmail.com>
2023-10-19 16:45:39 +0800

    Did test on different convulution networks

--- models.py
+++ models.py
@@ -2,27 +2,28 @@ import torch.nn as nn
 
 class CNN(nn.Module):
     
-    def __init__(self):
+    def __init__(self, dropout = 0.25, kernel_size = 3):
         super(CNN, self).__init__()
         
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernel_size, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
         
         self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
-        self.drop = nn.Dropout2d(0.25)
-        self.fc2 = nn.Linear(in_features=600, out_features=120)
-        self.fc3 = nn.Linear(in_features=120, out_features=10)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(in_features=600, out_features=300)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc3 = nn.Linear(in_features=300, out_features=10)
         
     def forward(self, x):
         out = self.layer1(x)
@@ -34,3 +35,225 @@ class CNN(nn.Module):
         out = self.fc3(out)
         
         return out
+    
+class CNN_no_batch(nn.Module):
+    
+    def __init__(self, dropout = 0.25, kernel_size = 3):
+        super(CNN_no_batch, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernel_size, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(in_features=600, out_features=300)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc3 = nn.Linear(in_features=300, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
+    
+class CNN_no_maxpool(nn.Module):
+    
+    def __init__(self, dropout = 0.25, kernel_size = 3):
+        super(CNN_no_maxpool, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernel_size, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        
+        self.fc1 = nn.Linear(in_features=64*26*26, out_features=600)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(in_features=600, out_features=300)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc3 = nn.Linear(in_features=300, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
+    
+class CNN_no_maxpool_batchnorm(nn.Module):
+    
+    def __init__(self, dropout = 0.25, kernel_size = 3):
+        super(CNN_no_maxpool_batchnorm, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernel_size, padding=1),
+            nn.ReLU(),
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size),
+            nn.ReLU(),
+        )
+        
+        self.fc1 = nn.Linear(in_features=64*26*26, out_features=600)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(in_features=600, out_features=300)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc3 = nn.Linear(in_features=300, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
+    
+
+class CNN_kernel_check(nn.Module):
+    
+    def __init__(self, dropout = 0.25, kernel_size = 3):
+        super(CNN_kernel_check, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernel_size, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        
+        self.fc1 = nn.Linear(in_features=576, out_features=600)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(in_features=600, out_features=300)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc3 = nn.Linear(in_features=300, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
+    
+class CNN_conv_layer(nn.Module):
+    
+    def __init__(self, dropout = 0.25, kernel_size = 3):
+        super(CNN_conv_layer, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernel_size, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=kernel_size),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=kernel_size),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        
+        self.fc1 = nn.Linear(in_features=6272, out_features=600)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(in_features=600, out_features=300)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc3 = nn.Linear(in_features=300, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        #out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
+
+
+class CNN_fully_connected(nn.Module):
+    
+    def __init__(self, kernel_size = 3, dropout = 0.25):
+        super(CNN_fully_connected, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernel_size, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+        self.drop = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(in_features=600, out_features=300)
+        self.fc3 = nn.Linear(in_features=300, out_features=300)
+        self.fc4 = nn.Linear(in_features=300, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.drop(out)
+        out = self.fc3(out)
+        out = self.drop(out)
+        # out = self.fc3(out)
+        # out = self.drop(out)
+        out = self.fc4(out)
+        
+        return out
+    
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+from torch.autograd import Variable
 
 def output_label(label):
     output_mapping = {0: "T-shirt/Top",
@@ -13,3 +14,57 @@ def output_label(label):
                     9: "Ankle Boot"}
     input = (label.item() if type(label) == torch.Tensor else label)
     return output_mapping[input]
+
+def train_loop(train_loader, model, loss_fn, optimizer, device):
+    size = len(train_loader.dataset)
+    num_batches = len(train_loader)
+
+    train_loss, train_correct = 0, 0
+
+    for images, labels in train_loader:
+        # Transfering images and labels to GPU if available
+        images, labels = images.to(device), labels.to(device)
+        
+        # Forward pass 
+        outputs = model(images)
+        loss = loss_fn(outputs, labels)
+        
+        optimizer.zero_grad()
+        
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+
+        _, predicted = torch.max(outputs, dim=1)
+
+        train_correct += (predicted == labels).type(torch.float).sum().item()
+
+    train_loss /= num_batches
+    train_correct /=size
+    
+    return train_loss, train_correct
+
+
+def test_loop(test_loader, model, loss_fn, device):
+
+    size = len(test_loader.dataset)
+    num_batches = len(test_loader)
+    test_loss, test_correct = 0, 0
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+
+            test, labels = images.to(device), labels.to(device)
+
+            outputs = model(test)
+
+            test_loss += loss_fn(outputs, labels).item()
+
+            _, predicted = torch.max(outputs, dim=1)
+            test_correct += (predicted == labels).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    test_correct /= size
+    
+    return test_loss, test_correct
